012e91078b1b92723e156d513a0e7a8f14072040
mdcarron <mdcarron.dev@gmail.com>
2021-02-03 17:24:28 -0300

    Adds caesars cipher logic

--- script.rb
+++ script.rb
@@ -1,1 +1,28 @@
-puts "Hello World!"
+def caesar_cipher(str, key=3)
+    
+    ordinals = str.codepoints
+    shifted_ordinals = ordinals.map do |n| 
+        if n >= 65 && n <= 90
+            if n + key > 90
+                n + key - 26
+            else
+                n + key
+            end
+        elsif n >= 97 && n <= 122
+            if n + key > 122
+                n + key - 26
+            else
+                n + key
+            end
+        else
+            n
+        end    
+    end
+    
+    encrypted_msg = ""
+    shifted_ordinals.each { |n| encrypted_msg << n.chr }
+
+    return encrypted_msg
+end
+
+puts caesar_cipher("This is a common secret message", 7)
